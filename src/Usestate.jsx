95b67bc93bbc569d454761e5fdd13a83bafc8a43
sahulakki123 <lakkisahus04@gmail.com>
2025-10-08 16:40:38 +0530

    update

--- src/Usestate.jsx
+++ src/Usestate.jsx
@@ -1,20 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 
 const UseState=()=>{
 
-    let [count,setcount]=useState(0)
-    useEffect( ()=>{
+    // let [count,setcount]=useState(0)
+    // useEffect( ()=>{
 
-        console.log("Runnning");
+    //     console.log("Runnning");
         
-    },[])
+    // },[])
+
+
+    let inpRef=useRef()
+
+    let show=()=>{
+        console.log(inpRef.current.focus());
+        
+    }
 
     return(
         <>
-            <h1>{count}</h1>
+            {/* <h1>{count}</h1>
             <button onClick={()=>{setcount(count+1)}}>Increment</button>
-            <h1>UseState</h1>
+            <h1>UseState</h1> */}
+d
+
+            <h1>Useffect 1</h1>
+            <input type="text" ref={inpRef}/>
+            <button onClick={show}>Show</button>
         </>
     )
 }
